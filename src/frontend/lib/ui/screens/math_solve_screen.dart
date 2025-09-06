5180698ffbebd11317a6ac850125ac1acecce091
DESKTOP-UJTRUVQ\Admin <mcuongtrinh96@gmail.com>
2025-09-06 10:21:56 +0700

    init flutter prj  + add image piker dependency + add permission

---
+++ src/frontend/lib/ui/screens/math_solve_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MathSolveScreen extends StatelessWidget {
+  const MathSolveScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Math Assistant"),
+      ),
+      body: const SizedBox(),
+    );
+  }
+}
