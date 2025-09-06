2afbfcb3f586d9e6a91e1c48cbe22304e50eb7c0
DESKTOP-UJTRUVQ\Admin <mcuongtrinh96@gmail.com>
2025-09-06 10:10:46 +0700

    init class

---
+++ src/backend/managers/math_assist_manager.py
@@ -0,0 +1,5 @@
+
+class MathAssitManager: 
+    
+    def __init__(self):
+        pass
---
+++ src/backend/services/multimodal_service.py
@@ -0,0 +1,8 @@
+
+import os
+from openai import AzureOpenAI 
+
+class MultimodalService: 
+    
+    def __init__(self):
+        pass
